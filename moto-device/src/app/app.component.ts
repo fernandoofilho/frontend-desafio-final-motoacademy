65d675fce97957f6ee549c80b17c73708b439c52
franciscocmneto <fdcmn.snf20@uea.edu.br>
2025-01-27 16:40:51 -0400

    search-bar added

--- moto-device/src/app/app.component.ts
+++ moto-device/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
