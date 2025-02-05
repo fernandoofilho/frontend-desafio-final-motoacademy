8add92847d3540ba50679bba24a2283c85a31756
franciscocmneto <90157001+franciscocmneto@users.noreply.github.com>
2025-02-05 18:14:17 -0400

    Dev (#3)
    * project created
    
    * search-bar added
    
    * navbar searchbar

--- moto-device/src/app/app.routes.ts
+++ moto-device/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+// import { SearchPageComponent } from './pages/search-page/search-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'home', component: HomePageComponent},
+  // { path: 'search', component: SearchPageComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+];
