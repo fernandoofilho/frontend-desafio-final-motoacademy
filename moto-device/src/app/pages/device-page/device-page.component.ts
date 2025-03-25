0e30e94031afbab5c0299b3a0aab5f86dbffe4c0
Fernando Filho <faaf.eng19@uea.edu.br>
2025-03-24 23:27:46 -0400

    Feat/ai search (#13)
    * feat: device page
    
    * Feat/ai search (#12)
    
    * Feat/api service + proxy config (#4)
    
    * project created
    
    * search-bar added
    
    * Dev (#3)
    
    * project created
    
    * search-bar added
    
    * navbar searchbar
    
    * add service to comunicate with api
    
    * chore/add proxy config to angular.json
    
    ---------
    
    Co-authored-by: franciscocmneto <fdcmn.snf20@uea.edu.br>
    Co-authored-by: franciscocmneto <90157001+franciscocmneto@users.noreply.github.com>
    Co-authored-by: Fernando Filho <fernando.alves@indt.org.br>
    
    * Dev (#5)
    
    * project created
    
    * search-bar added
    
    * navbar searchbar
    
    * Dev (#3)
    
    * project created
    
    * search-bar added
    
    * navbar searchbar
    
    * add service to comunicate with api
    
    * chore/add proxy config to angular.json
    
    * add filter services
    
    ---------
    
    Co-authored-by: Fernando Filho <faaf.eng19@uea.edu.br>
    Co-authored-by: Fernando Filho <fernando.alves@indt.org.br>
    
    * Revert "Dev (#5)" (#6)
    
    This reverts commit 09c9cdbbf5de95fd6c64c9741321a5b735993740.
    
    * Dev (#7)
    
    * project created
    
    * search-bar added
    
    * navbar searchbar
    
    * Dev (#3)
    
    * project created
    
    * search-bar added
    
    * navbar searchbar
    
    * add service to comunicate with api
    
    * chore/add proxy config to angular.json
    
    * add filter services
    
    ---------
    
    Co-authored-by: Fernando Filho <faaf.eng19@uea.edu.br>
    Co-authored-by: Fernando Filho <fernando.alves@indt.org.br>
    
    * feat/ team page + device card + dialog
    
    * fix
    
    * add filters
    
    * fork sync
    
    * feat/skeleton loading
    
    * feat/create new filters services +function to getSrc + updating model
    
    * feat/init filters components + export feature
    
    * feat/filters
    
    * feat/change phone or watch
    
    * feat/AI filter mock
    
    * feat/improve styles
    
    * feat/add AI features + device page
    
    * feat: device page
    
    ---------
    
    Co-authored-by: franciscocmneto <fdcmn.snf20@uea.edu.br>
    Co-authored-by: franciscocmneto <90157001+franciscocmneto@users.noreply.github.com>
    Co-authored-by: Fernando Filho <fernando.alves@indt.org.br>
    
    * fix pdf image
    
    * feat: WIP compare phones + feat youtube search
    
    ---------
    
    Co-authored-by: Fernando Filho <fernando.alves@indt.org.br>
    Co-authored-by: franciscocmneto <fdcmn.snf20@uea.edu.br>
    Co-authored-by: franciscocmneto <90157001+franciscocmneto@users.noreply.github.com>

--- moto-device/src/app/pages/device-page/device-page.component.ts
+++ moto-device/src/app/pages/device-page/device-page.component.ts
@@ -6,11 +6,15 @@ import {
 } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute } from '@angular/router';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 import { Device } from '../../../shared/models/device.model';
 import { IntelligenceBottomSheetComponent } from '../../components/intelligence-bottom-sheet/intelligence-bottom-sheet.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { ApiService } from '../../services/api.service';
 import getSrc from '../../shared/functions/get_src';
+import { getImageDimensions } from '../../shared/functions/getImageDimensions';
+
 @Component({
   selector: 'app-device-page',
   standalone: true,
@@ -29,6 +33,7 @@ export class DevicePageComponent implements OnInit {
   DeviceDescription: { [x: string]: string } = {};
   isLoading: boolean = false;
   private _bottomSheet = inject(MatBottomSheet);
+  imageDimensions: { width: number; height: number } = { width: 0, height: 0 };
 
   openBottomSheet(): void {
     this._bottomSheet.open(IntelligenceBottomSheetComponent, {
@@ -57,5 +62,60 @@ export class DevicePageComponent implements OnInit {
 
   getImageLink(path: string) {
     return getSrc(path);
+  }
+  generatePDF() {
+    const element = document.getElementById('containerPage');
+
+    if (!element) {
+      console.error('Elemento não encontrado!');
+      return;
+    }
+
+    html2canvas(element, { scale: 4 }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const pageWidth = pdf.internal.pageSize.getWidth();
+      const pageHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = pageWidth - 10;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const imglink = this.getImageLink(this.deviceData?.src || '');
+      getImageDimensions(imglink)
+        .then((dimensions) => {
+          let imgDeviceWidth = dimensions.width;
+          let imgDeviceHeight = dimensions.height;
+
+          if (imgDeviceHeight > 100) {
+            const ratio = 100 / imgDeviceHeight;
+            imgDeviceHeight = 100;
+            imgDeviceWidth *= ratio;
+          }
+
+          const centerX = (pageWidth - imgDeviceWidth) / 2;
+
+          const imageTopMargin = 10;
+          const contentTopMargin = imageTopMargin + imgDeviceHeight + 10;
+
+          pdf.addImage(
+            imglink,
+            'JPG',
+            centerX,
+            imageTopMargin,
+            imgDeviceWidth,
+            imgDeviceHeight
+          );
+
+          pdf.addImage(
+            imgData,
+            'PNG',
+            5,
+            contentTopMargin,
+            imgWidth,
+            imgHeight
+          );
+
+          pdf.save('Report.pdf');
+        })
+        .catch((error) => console.error('Erro ao carregar a imagem', error));
+    });
   }
 }
